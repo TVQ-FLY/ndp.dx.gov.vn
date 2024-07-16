36effdc438cd45e4a3a505dff5b7caf5ab0bc95d
Quyền Trần Văn <tranvanquyenx4@gmail.com>
2024-07-16 10:24:36 +0700

    add js

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
+const btnOpen = document.querySelector('.search-navbar .open-menu')
+const menu = document.querySelector('.menu-mobile')
+const btnClose = document.querySelector('.search-navbar .close-menu')
 
-        
+btnOpen.onclick = ()=>{
+    menu.classList.remove('translate-x-full')
+    btnClose.classList.remove('hidden')
+    btnOpen.classList.add('hidden')
+}
+btnClose.onclick = ()=>{
+    menu.classList.add('translate-x-full')
+    btnClose.classList.add('hidden')
+    btnOpen.classList.remove('hidden')
+}
